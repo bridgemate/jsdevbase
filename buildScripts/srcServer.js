591f5db9b5b1447367e7dc8bf77f868237b1bc55
Ken Wu <folksbridge@gmail.com>
2019-12-20 18:12:11 -0500

    Integrate webpack into express

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -8,8 +8,8 @@ import open from 'open';
 // const open = require('open');
 
 //*after configuring in config file, set up dev server to serve webpack bundle
-// import webpack from 'webpack';
-// import config from '../webpack.config.dev';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
 /* eslint-disable no-console */
 
@@ -17,11 +17,11 @@ const port = 3000;
 const app = express();
 
 //*tells express to use webpack middleware
-// const compiler = webpack(config);
-// app.use(require('webpack-dev-middleware')(compiler, {
-//   noInfo: true,
-//   publicPath: config.output.publicPath
-// }));
+const compiler = webpack(config);
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'))
